f1921ca7ae8a5990d127a371d618a32f2246d8a1
3096 <the3096@gmail.com>
2015-12-27 16:14:30 -0800

    repaired the output order and use methods of Scanner instead of StringTokenizer

--- src/gift1.java
+++ src/gift1.java
@@ -11,35 +11,37 @@ class gift1 {
   public static void main (String [] args) throws IOException {
     Scanner s = new Scanner(new FileReader("gift1.in"));	// input file to Scanner
     PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));	//output file
-    // Get the String of the whole file from Scanner, break into tokens
-    StringTokenizer st = new StringTokenizer(s.useDelimiter("\\Z").next());
     
-    int np = Integer.parseInt(st.nextToken());    // NP Count
+    int np = s.nextInt();    // NP Count
     
     //Initialize Givers ArrayList
     HashMap<String, Giver> npg = new HashMap<String, Giver>();
     for(int i = 0; i < np; i++){
-    	String name = st.nextToken();
+    	String name = s.next();
     	npg.put( name, new Giver(name) );
     }
     
     //Loops through the whole money giving
     for(int i = 0; i < np; i++){
-    	Giver cGiver = npg.get(st.nextToken());		//Current giver
-    	int cMoney = Integer.parseInt(st.nextToken()),		//Current giver's money
-    		cRecipient = Integer.parseInt(st.nextToken());	//Current recipient count
+    	Giver cGiver = npg.get(s.next());		//Current giver
+    	int cMoney = s.nextInt(),		//Current giver's money
+    		cRecipient = s.nextInt();	//Current recipient count
     	int cGiving = 0;
     	if(cRecipient != 0)
     		cGiving = cMoney/cRecipient;					//Money giving to each recipient
     	cGiver.setInitMoney(cMoney);						//Set initial money for comparison.
     	cGiver.setMoney(0 - cGiving*cRecipient);			//Give away money.
     	for(int j = 0; j < cRecipient; j++){
-    		npg.get(st.nextToken()).setMoney(cGiving);	//Each recipient receive money
+    		npg.get(s.next()).setMoney(cGiving);	//Each recipient receive money
     	}	
     }
     
-    for(Giver g : npg.values())
+    s = new Scanner(new FileReader("gift1.in"));	// new scanner, yeah!
+    s.next();										// skip to the names
+    for(int i = 0; i<npg.size(); i++){
+    	Giver g = npg.get(s.next());
     	out.println(g.name +" "+ (g.money-g.iMoney));  // output result
+    }
     out.close();                                  // close the output file
     s.close();									  // close the BufferedReader
   }
