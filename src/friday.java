fb31e46d2b2265bc4c645b0cab7797505845bf3b
3096 <the3096@gmail.com>
2015-12-27 21:03:36 -0800

    Commit for the program "friday".

---
+++ src/friday.java
@@ -0,0 +1,63 @@
+
+/*
+ID: yao.dai1
+LANG: JAVA
+TASK: friday
+*/
+import java.io.*;
+import java.util.*;
+
+public class friday {
+
+	public static void main(String[] args) throws IOException {
+
+		// Initialize i/o
+		Scanner sc = new Scanner(new FileReader("friday.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
+
+		// Some variables initialization
+		int years = sc.nextInt();
+		int[] thirteenth = new int[7]; // the array that counts each day
+		int[] days = { 31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int today = 4; // last day of the thirteenth
+		int y = 1900; // current year
+
+		// Loop through each year
+		for (int i = 0; i < years; i++) {
+			// Determine the days of February
+			if (leap(y))
+				days[2] = 29;
+			else
+				days[2] = 28;
+			// Loop through each month
+			for (int m = 1; m <= 12; m++) {
+				today = (today + (days[m - 1] % 7)) % 7; // Determine the day of
+															// this month's 13th
+				thirteenth[today]++; // Count day in the array
+			}
+			y++; // Increase current year
+		}
+
+		String output = ""; // String holder for output result
+		for (int n : thirteenth)
+			output += n + " ";
+		out.println(output.trim()); // Come on USACO, I didn't have to do this
+									// last time!
+		sc.close();
+		out.close();
+	}
+
+	// method for leap year
+	static boolean leap(int year) {
+		if (year % 4 == 0)
+			if (year % 100 == 0)
+				if (year % 400 == 0)
+					return true;
+				else
+					return false;
+			else
+				return true;
+
+		return false;
+	}
+}
--- src/gift1.java
+++ src/gift1.java
@@ -1,69 +1,76 @@
+
 /*
 ID: yao.dai1
 LANG: JAVA
 TASK: gift1
 */
-import java.io.*;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Scanner;
 
 class gift1 {
 
-  public static void main (String [] args) throws IOException {
-    Scanner s = new Scanner(new FileReader("gift1.in"));	// input file to Scanner
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));	//output file
-    
-    int np = s.nextInt();    // NP Count
-    
-    //Initialize Givers ArrayList
-    HashMap<String, Giver> npg = new HashMap<String, Giver>();
-    for(int i = 0; i < np; i++){
-    	String name = s.next();
-    	npg.put( name, new Giver(name) );
-    }
-    
-    //Loops through the whole money giving
-    for(int i = 0; i < np; i++){
-    	Giver cGiver = npg.get(s.next());		//Current giver
-    	int cMoney = s.nextInt(),		//Current giver's money
-    		cRecipient = s.nextInt();	//Current recipient count
-    	int cGiving = 0;
-    	if(cRecipient != 0)
-    		cGiving = cMoney/cRecipient;					//Money giving to each recipient
-    	cGiver.setInitMoney(cMoney);						//Set initial money for comparison.
-    	cGiver.setMoney(0 - cGiving*cRecipient);			//Give away money.
-    	for(int j = 0; j < cRecipient; j++){
-    		npg.get(s.next()).setMoney(cGiving);	//Each recipient receive money
-    	}	
-    }
-    
-    s = new Scanner(new FileReader("gift1.in"));	// new scanner, yeah!
-    s.next();										// skip to the names
-    for(int i = 0; i<npg.size(); i++){
-    	Giver g = npg.get(s.next());
-    	out.println(g.name +" "+ (g.money-g.iMoney));  // output result
-    }
-    out.close();                                  // close the output file
-    s.close();									  // close the BufferedReader
-  }
-  
-    static class Giver{
-		String name; //The person's name
-		int money;   //The person's money
-		int iMoney;	 //The person's initial money
-		
-		//Constructor
-		Giver(String name){
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(new FileReader("gift1.in")); // input file to Scanner
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out"))); // output file
+
+		int np = s.nextInt(); // NP Count
+
+		// Initialize Givers ArrayList
+		HashMap<String, Giver> npg = new HashMap<String, Giver>();
+		for (int i = 0; i < np; i++) {
+			String name = s.next();
+			npg.put(name, new Giver(name));
+		}
+
+		// Loops through the whole money giving
+		for (int i = 0; i < np; i++) {
+			Giver cGiver = npg.get(s.next()); // Current giver
+			int cMoney = s.nextInt(), // Current giver's money
+					cRecipient = s.nextInt(); // Current recipient count
+			int cGiving = 0;
+			if (cRecipient != 0)
+				cGiving = cMoney / cRecipient; // Money giving to each recipient
+			cGiver.setInitMoney(cMoney); // Set initial money for comparison.
+			cGiver.setMoney(0 - cGiving * cRecipient); // Give away money.
+			for (int j = 0; j < cRecipient; j++) {
+				npg.get(s.next()).setMoney(cGiving); // Each recipient receive
+														// money
+			}
+		}
+
+		s = new Scanner(new FileReader("gift1.in")); // new scanner, yeah!
+		s.next(); // skip to the names
+		for (int i = 0; i < npg.size(); i++) {
+			Giver g = npg.get(s.next());
+			out.println(g.name + " " + (g.money - g.iMoney)); // output result
+		}
+		out.close(); // close the output file
+		s.close(); // close the BufferedReader
+	}
+
+	static class Giver {
+		String name; // The person's name
+		int money; // The person's money
+		int iMoney; // The person's initial money
+
+		// Constructor
+		Giver(String name) {
 			this.name = name;
-			money = 0; 
+			money = 0;
 			iMoney = 0;
 		}
 
-		//Money modifier
-		void setMoney(int money){
+		// Money modifier
+		void setMoney(int money) {
 			this.money += money;
 		}
-		
-		void setInitMoney(int money){
+
+		void setInitMoney(int money) {
 			iMoney = money;
 			setMoney(money);
 		}
